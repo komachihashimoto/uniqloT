0995a11bd2974e39700320b6107cec622f2ca4d6
komachihashimoto <joujinameko@gmail.com>
2024-01-14 20:04:20 +0900

    答え合わせ

--- uniqloT.go
+++ uniqloT.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"html"
+	"log"
 	"net/http"
 	"strconv"
+	"time"
 )
 
 type ControlMessage struct {
@@ -15,47 +17,66 @@ type ControlMessage struct {
 func main() {
 	controlChannel := make(chan ControlMessage)
 	workerCompleteChan := make(chan bool)
-	statusPollChannel := make(chan chan bool)
+	pollChannel := make(chan chan bool)
 	workerActive := false
 
-	go admin(controlChannel, statusPollChannel)
-
-	for {
-		select {
-		case respChan := <-statusPollChannel:
-			respChan <- workerActive
-			workerActive = true
-		case status := <-workerCompleteChan:
-			workerActive = status
+	go func() {
+		for {
+			select {
+			case respChan := <-pollChannel:
+				respChan <- workerActive
+			case msg := <-controlChannel:
+				workerActive = true
+				go doStuff(msg, workerCompleteChan)
+			case status := <-workerCompleteChan:
+				workerActive = status
+			}
 		}
-	}
-}
+	}()
 
-func admin(cc chan ControlMessage, statusPollChannel chan chan bool) {
 	http.HandleFunc("/admin", func(w http.ResponseWriter, r *http.Request) {
 		r.ParseForm()
 		count, err := strconv.ParseInt(r.FormValue("count"), 10, 32)
 		if err != nil {
-			fmt.Fprint(w, err.Error())
+			log.Printf("Error parsing count: %v", err)
 			return
 		}
-		msg := ControlMessage{Target: r.FormValue("target"), Count: int(count)}
-		cc <- msg
-		fmt.Fprintf(w, "Control message issued for Target: %s, Count: %d", html.EscapeString(r.FormValue("target")), count)
+		msg := ControlMessage{
+			Target: r.FormValue("target"),
+			Count:  int(count),
+		}
+		controlChannel <- msg
+		fmt.Fprintf(w, "Control message issued for Target %s\n", html.EscapeString(r.FormValue("target")))
 	})
 
 	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
 		reqChan := make(chan bool)
-		statusPollChannel <- reqChan
+		pollChannel <- reqChan
+		timeout := time.NewTimer(5 * time.Second)
 		select {
 		case result := <-reqChan:
 			if result {
-				fmt.Fprint(w, "ACTIVE")
+				fmt.Fprintln(w, "ACTIVE")
 			} else {
-				fmt.Fprint(w, "INACTIVE")
+				fmt.Fprintln(w, "INACTIVE")
 			}
+		case <-timeout.C:
+			fmt.Fprintln(w, "TIMEOUT")
 		}
 	})
 
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func doStuff(msg ControlMessage, completeChan chan<- bool) {
+	defer func() {
+		completeChan <- false
+	}()
+
+	for i := 0; i < msg.Count; i++ {
+		fmt.Printf("Target: %s, Count: %d\n", msg.Target, i)
+		time.Sleep(time.Second)
+	}
+
+	completeChan <- true
 }
